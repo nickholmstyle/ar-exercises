8986c4e8b1c5714b8809bb6abac8d46fe21de36d
nickholmstyle <nick416holm@protonmail.com>
2022-10-17 20:52:01 +0000

    commit exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.second
+
+@store1.name = "Leslieville"
+
+puts @store1.name
+puts @store2.name
