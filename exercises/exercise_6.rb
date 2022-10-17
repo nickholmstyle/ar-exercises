8f2f519e4ddf6c4b2cab688c04a3140736499c44
nickholmstyle <nick416holm@protonmail.com>
2022-10-17 22:31:08 +0000

    commit exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Nick", last_name: "Holm", hourly_rate: 60)
+@store1.employees.create(first_name: "Bonita", last_name: "Chihuahua", hourly_rate: 100)
+@store2.employees.create(first_name: "Kaz", last_name: "Branscombe", hourly_rate: 100)
+@store2.employees.create(first_name: "Suzanne", last_name: "Gerweck", hourly_rate: 200)
